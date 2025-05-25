0fa2ac3d0b9cbe1e33f50fd963449fd2777532b8
m4zeboy <moises.azevedo@ufms.br>
2025-05-25 15:25:40 -0300

    implement WebSocket upgrade handling and handshake process

--- nodejs-raw-web-socket/server.js
+++ nodejs-raw-web-socket/server.js
@@ -1,15 +1,44 @@
+import crypto from 'node:crypto';
 import { createServer } from 'node:http';
-
 const PORT = 1337;
+const WEBSOCKET_MAGIC_STRING_KEY = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11';
 
 const server = createServer((request, response) => {
 	response.writeHead(200);
 	response.end('hy there.');
 }).listen(PORT, () => console.log('server running at ', PORT));
 
-server.on('upgrade', (req, socket, head) => {
-	console.log({ req, socket, head });
-});
+server.on('upgrade', onSocketUpgrade);
+
+function onSocketUpgrade(req, socket, head) {
+	// sec-websocket-key
+	const { 'sec-websocket-key': webClientSocketKey } = req.headers;
+
+	console.log(`${webClientSocketKey} connected.`);
+
+	const headers = prepareHandshakeHeaders(webClientSocketKey);
+	socket.write(headers);
+}
+
+function prepareHandshakeHeaders(id) {
+	const acceptKey = createSocketAccept(id);
+	const headers = [
+		'HTTP/1.1 101 Switching Protocols',
+		'Upgrade: websocket',
+		'Connection: Upgrade',
+		`Sec-WebSocket-Accept: ${acceptKey}`,
+		'',
+	]
+		.map((line) => line.concat('\r\n'))
+		.join('');
+	return headers;
+}
+
+function createSocketAccept(id) {
+	const shaone = crypto.createHash('sha-1');
+	shaone.update(id + WEBSOCKET_MAGIC_STRING_KEY);
+	return shaone.digest('base64');
+}
 
 errorHandling();
 
